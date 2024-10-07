36d2d3c8d0778a974bbc0b77ef6a52d1f7b7f6f9
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-07 18:18:56 -0400

    Create h_e.vb

---
+++ h_e.vb
@@ -0,0 +1,7 @@
+Imports System 'System is a Namespace  
+Module H_E  
+    Sub Main()  
+        Console.WriteLine("Deduce away from a score of 100: weekly; for every hour exercised below 7, for every hour slept below 56, for every 125 calories away from 2000").  
+        Console.ReadKey()  
+    End Sub  
+End Module  
